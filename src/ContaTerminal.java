ecf31e32dbbcead0910fdb54c478040aa229ae1b
Fernando Henrique Brambilla <95035507+FernandoBrambilla@users.noreply.github.com>
2024-05-25 09:28:17 -0300

    Update ContaTerminal.java
    correção

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -20,9 +20,9 @@ public class ContaTerminal {
 		scan.close();
 		
 		System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, "
-				+ "sua agência é " + agencia +" +, "
-						+ "conta " + numero + " e seu saldo "
-								+ saldo + " já está disponível para saque");
+				+ "sua agência é " + agencia +", "
+						+ "conta " + numero + " e seu saldo de R$ "
+								+ saldo + " já está disponível para saque.");
 	}
 
 }
